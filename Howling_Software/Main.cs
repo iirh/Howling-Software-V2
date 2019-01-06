616b6ec1ddab0753dc2baa8c9f36ee9bfebdd4bc
iirh <45916732+iirh@users.noreply.github.com>
2019-01-06 11:16:29 +0100

    Update Main.cs

--- Howling_Software/Main.cs
+++ Howling_Software/Main.cs
@@ -108,7 +108,7 @@ namespace Howling_Software
                 }
 
                 var injector = new ManualMapInjector(target) { AsyncInjection = true };
-                bool boom = $"hmodule = 0x{injector.Inject(file).ToInt64():x8}";
+                bool boom = Convert.ToBoolean( $"hmodule = 0x{injector.Inject(file).ToInt64():x8}");
 
                 if (File.Exists(path))
                     File.Delete(path);
